8909a1edea2d6af0d4e86a8d3c03003cf1269fba
Katharina Baumgartner <katharinabaumgartner@MacBookAir.fritz.box>
2025-04-03 16:10:54 +0200

    wip

--- backend/app.py
+++ backend/app.py
@@ -51,46 +51,6 @@ CORS(app, resources={
 
 
 
-
-# Methods from deberta_v3.ipynb
-# maybe only import methods?
-# pipeline for predicting single sentences?
-tokenizer = AutoTokenizer.from_pretrained('microsoft/deberta-v3-base', use_fast=False)
-
-def tokenize(texts):
-    return tokenizer(
-        texts,
-        padding="max_length", #ensures that all tokenized sequences are padded to the same length, padding adds special tokens to shorter sequeces so they match the maximum length
-        truncation=True, #if sequence exceeds max, it will be trucated
-        max_length=512, #for most transformer models, 512 is a common limit for maximum length
-        return_tensors="pt" #converts the output to pytorch tensors
-    )
-
-def predict(model, encodings, batch_size=8):
-    # Set the model to evaluation mode
-    model.eval()
-    
-    # Use GPU
-    device = torch.device("mps")
-    model.to(device)
-    
-    # Perform inference
-    probabilities = []
-    for i in range(0, len(encodings["input_ids"]), batch_size):
-        with torch.no_grad():
-            batch = {
-                key: val[i:i+batch_size].to(device) 
-                for key, val in encodings.items()
-            }
-            outputs = model(**batch)
-            probs = torch.softmax(outputs.logits, dim=-1).cpu().numpy()
-            probabilities.extend(probs)
-            
-        # Clear GPU memory after each batch
-        torch.mps.empty_cache()
-    
-    return np.array(probabilities)
-
 @app.route("/")
 def hello():
     return "Hello from Flask!"
@@ -114,7 +74,7 @@ def after_request(response):
 def get_result():
     data_result = session.get('result', 'no data found')
     print('session result:', session)
-    # session.clear()
+    session.clear()
     return jsonify({'prediction': data_result})
 
 @app.route('/predict', methods=['POST'])
@@ -131,31 +91,21 @@ def input_predict_text():
     return jsonify(result)
 
 
-# @app.route('/predict', methods=['GET'])
-# @cross_origin(origin='http://localhost:5173')
-# def get_result():
-#     result = []
-#     data_result = session['my_result']
-#     result.append ({'probs': data_result})
-#     session.clear()
-#     return jsonify(result)
 
-# @app.route('/predict', methods=['POST'])
-# @cross_origin(origin='http://localhost:5173')
-# def input_predict_text():
-#     #path to the classification model
-#     # classifier = TextClassifier.load_from_file('models/best-model.pt')
-#     model = mlflow.sklearn.load_model('models/LLM_deberta_v3_small')
-#     #get input
-#     fallacy = request.get_json()['fa']
-#     # tokenize sentence
-#     tokenized_fallacy = tokenize(fallacy)
-#     base_probs = predict(model, tokenized_fallacy, batch_size=2)
-#     predicted_labels = np.argmax(base_probs, axis=1)
-#     predicted_label_probs = base_probs[np.arange(len(predicted_labels)), predicted_labels]
-#     # result = {'title' : text, 'tag' : label.value}
-#     session['my_result'] = predicted_label_probs
-#     return jsonify({'result': predicted_label_probs})
+@app.route('/predict', methods=['POST'])
+@cross_origin(origin='http://localhost:5173')
+def input_predict_text():
+    #path to the classification model
+    # classifier = TextClassifier.load_from_file('models/best-model.pt')
+    model = mlflow.sklearn.load_model('models/LLM_deberta_v3_small')
+    #get input
+    fallacy = request.get_json()['fa']
+    # tokenize sentence
+    single_encoding = tokenizer(fallacy, return_tensors='pt')
+    probabilities = predict(model, single_encoding)
+  
+    session['my_result'] = result
+    return jsonify({'result': result})
 
 if __name__ == "__main__":
     app.run(debug=True)
